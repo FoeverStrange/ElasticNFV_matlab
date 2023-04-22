bafec39d92984f114be0a45719c77fdf54dbc340
Qunboo <qunboo@example.com>
2023-04-22 15:09:18 +0800

    4022

--- RandChoose_Elastic.m
+++ RandChoose_Elastic.m
@@ -6,11 +6,35 @@ serviceChain_vec = serviceChainCell(row,:);
 % 1：NFV数量；2：部署在哪些PM上了；3：NFV需要的CPU数；4：需要的Mem数；
 % 5：NFV间带宽需求；6：业务生命周期；7：业务链初始编号
 
+%     访问对应NFV的资源数量
+NFVCPU_vec = serviceChain_vec{3};
+NFVCPUNum = NFVCPU_vec(NFVNo);
+if elasticNo == 1
+   NFVCPUNum = NFVnewXXXNum;
+end
+NFVMem_vec = serviceChain_vec{4};
+NFVMemNum = NFVMem_vec(NFVNo);
+if elasticNo == 3
+   NFVMemNum = NFVnewXXXNum; 
+end
+NFVBnd_vec = serviceChain_vec{5};
+NFVBnd_from = NFVBnd_vec(NFVNo);
+NFVBnd_to = NFVBnd_vec(NFVNo + 1);
+if elasticNo == 5
+   NFVBnd_from = NFVnewXXXNum; 
+   在Bnd迁移需求的时候，需要调度的包括IN和OUT
+end
 % elasticNo = 1, 3, 5 表示CPU，Mem，Bnd弹性需求
 for i = 1 : 10
 %     随机一个PM
     newPMNo = randi([1 PMNum]);
 %     检查这个PM资源是否能够承受NFV的资源
+    PM_CPUResource = CPUResource(newPMNo);
+    PM_MemResource = MemoryResource(newPMNo);
+    PM_BndResource_from = BandwidthResource(newPmNo);
+
+    
+    
     
 end
 
